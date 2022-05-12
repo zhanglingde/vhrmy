fc8fa2fdd21513252a90c79f969d017b43219a22
zhangling <390597591@qq.com>
2022-05-12 13:52:31 +0800

    添加 liquibase 控制数据库版本

---
+++ vhr/src/main/db/sql/init/init_nation.sql
@@ -0,0 +1,91 @@
+# 初始化民族信息
+INSERT INTO `nation` VALUES (1, '汉族');
+INSERT INTO `nation` VALUES (2, '蒙古族');
+INSERT INTO `nation` VALUES (3, '回族');
+INSERT INTO `nation` VALUES (4, '藏族');
+INSERT INTO `nation` VALUES (5, '维吾尔族');
+INSERT INTO `nation` VALUES (6, '苗族');
+INSERT INTO `nation` VALUES (7, '彝族');
+INSERT INTO `nation` VALUES (8, '壮族');
+INSERT INTO `nation` VALUES (9, '布依族');
+INSERT INTO `nation` VALUES (10, '朝鲜族');
+INSERT INTO `nation` VALUES (11, '满族');
+INSERT INTO `nation` VALUES (12, '侗族');
+INSERT INTO `nation` VALUES (13, '瑶族');
+INSERT INTO `nation` VALUES (14, '白族');
+INSERT INTO `nation` VALUES (15, '土家族');
+INSERT INTO `nation` VALUES (16, '哈尼族');
+INSERT INTO `nation` VALUES (17, '哈萨克族');
+INSERT INTO `nation` VALUES (18, '傣族');
+INSERT INTO `nation` VALUES (19, '黎族');
+INSERT INTO `nation` VALUES (20, '傈僳族');
+INSERT INTO `nation` VALUES (21, '佤族');
+INSERT INTO `nation` VALUES (22, '畲族');
+INSERT INTO `nation` VALUES (23, '高山族');
+INSERT INTO `nation` VALUES (24, '拉祜族');
+INSERT INTO `nation` VALUES (25, '水族');
+INSERT INTO `nation` VALUES (26, '东乡族');
+INSERT INTO `nation` VALUES (27, '纳西族');
+INSERT INTO `nation` VALUES (28, '景颇族');
+INSERT INTO `nation` VALUES (29, '柯尔克孜族');
+INSERT INTO `nation` VALUES (30, '土族');
+INSERT INTO `nation` VALUES (31, '达斡尔族');
+INSERT INTO `nation` VALUES (32, '仫佬族');
+INSERT INTO `nation` VALUES (33, '羌族');
+INSERT INTO `nation` VALUES (34, '布朗族');
+INSERT INTO `nation` VALUES (35, '撒拉族');
+INSERT INTO `nation` VALUES (36, '毛难族');
+INSERT INTO `nation` VALUES (37, '仡佬族');
+INSERT INTO `nation` VALUES (38, '锡伯族');
+INSERT INTO `nation` VALUES (39, '阿昌族');
+INSERT INTO `nation` VALUES (40, '普米族');
+INSERT INTO `nation` VALUES (41, '塔吉克族');
+INSERT INTO `nation` VALUES (42, '怒族');
+INSERT INTO `nation` VALUES (43, '乌孜别克族');
+INSERT INTO `nation` VALUES (44, '俄罗斯族');
+INSERT INTO `nation` VALUES (45, '鄂温克族');
+INSERT INTO `nation` VALUES (46, '崩龙族');
+INSERT INTO `nation` VALUES (47, '保安族');
+INSERT INTO `nation` VALUES (48, '裕固族');
+INSERT INTO `nation` VALUES (49, '京族');
+INSERT INTO `nation` VALUES (50, '塔塔尔族');
+INSERT INTO `nation` VALUES (51, '独龙族');
+INSERT INTO `nation` VALUES (52, '鄂伦春族');
+INSERT INTO `nation` VALUES (53, '赫哲族');
+INSERT INTO `nation` VALUES (54, '门巴族');
+INSERT INTO `nation` VALUES (55, '珞巴族');
+INSERT INTO `nation` VALUES (56, '基诺族');
+
+# 初始化政治面貌
+INSERT INTO `politics_status` VALUES (1, '中共党员');
+INSERT INTO `politics_status` VALUES (2, '中共预备党员');
+INSERT INTO `politics_status` VALUES (3, '共青团员');
+INSERT INTO `politics_status` VALUES (4, '民革党员');
+INSERT INTO `politics_status` VALUES (5, '民盟盟员');
+INSERT INTO `politics_status` VALUES (6, '民建会员');
+INSERT INTO `politics_status` VALUES (7, '民进会员');
+INSERT INTO `politics_status` VALUES (8, '农工党党员');
+INSERT INTO `politics_status` VALUES (9, '致公党党员');
+INSERT INTO `politics_status` VALUES (10, '九三学社社员');
+INSERT INTO `politics_status` VALUES (11, '台盟盟员');
+INSERT INTO `politics_status` VALUES (12, '无党派民主人士');
+INSERT INTO `politics_status` VALUES (13, '普通公民');
+
+
+# 初始化职位信息
+INSERT INTO `position` VALUES (29, '技术总监', '2018-01-11 21:13:42', 1);
+INSERT INTO `position` VALUES (30, '运营总监', '2018-01-11 21:13:48', 1);
+INSERT INTO `position` VALUES (31, '市场总监', '2018-01-11 00:00:00', 1);
+INSERT INTO `position` VALUES (33, '研发工程师', '2018-01-14 00:00:00', 0);
+INSERT INTO `position` VALUES (34, '运维工程师', '2018-01-14 16:11:41', 1);
+INSERT INTO `position` VALUES (36, 'Java研发经理', '2019-10-01 00:00:00', 1);
+
+# 初始化职称
+INSERT INTO `job_level` VALUES (9, '教授', '正高级', '2018-01-11 00:00:00', 1);
+INSERT INTO `job_level` VALUES (10, '副教授', '副高级', '2018-01-11 21:19:20', 1);
+INSERT INTO `job_level` VALUES (12, '助教', '初级', '2018-01-11 21:35:39', 1);
+INSERT INTO `job_level` VALUES (13, '讲师', '中级', '2018-01-11 00:00:00', 0);
+INSERT INTO `job_level` VALUES (14, '初级工程师', '初级', '2018-01-14 00:00:00', 1);
+INSERT INTO `job_level` VALUES (15, '中级工程师66', '中级', '2018-01-14 00:00:00', 1);
+INSERT INTO `job_level` VALUES (16, '高级工程师', '副高级', '2018-01-14 16:19:14', 1);
+INSERT INTO `job_level` VALUES (17, '骨灰级工程师', '正高级', '2018-01-14 16:19:24', 1);
---
+++ vhr/src/main/db/update.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+## 需要在本地liquibase的lib中添加mysql驱动  witch liquibase
+#liquibase   \
+#    --driver=com.mysql.cj.jdbc.Driver       \
+#    --changeLogFile=driver.xml              \
+#    --url=jdbc:mysql://192.168.95.142:3306/maycur-finance-operation-platform  \
+#    --username=plat_app   \
+#    --password=Maycur@2021 \
+#    --log-level=DEBUG \
+#   update
+
+
+# zhangling 本地  witch liquibase
+#liquibase \
+#  --driver=com.mysql.cj.jdbc.Driver \
+#  --changeLogFile=driver.xml \
+#  --url=jdbc:mysql://121.37.185.192:3306/flowable \
+#  --username=root \
+#  --password=zhangling03 \
+#  --log-level=DEBUG \
+#  update
+# zhangling 本地  witch liquibase
+liquibase \
+  --driver=com.mysql.cj.jdbc.Driver \
+  --changeLogFile=driver.xml \
+  --url=jdbc:mysql://127.0.0.1:3306/maycur-finance-operation-platform \
+  --username=root \
+  --password=zhangling03 \
+  --log-level=DEBUG \
+  update
