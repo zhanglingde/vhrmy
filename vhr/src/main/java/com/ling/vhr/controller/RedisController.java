bd4b25ea3c7f5432ca063716f7715ce83bb3346d
zhangling <390597591@qq.com>
2022-06-27 16:21:00 +0800

    【Redis限流】添加 redis 简单限流操作

---
+++ vhr/src/main/java/com/ling/vhr/controller/RedisController.java
@@ -0,0 +1,6 @@
+package com.ling.vhr.controller;/**
+ * @author zhangling
+ * @date 2022/6/27 4:14 PM
+ */
+public class RedisController {
+}
---
+++ vhr/src/test/java/com/ling/vhr/redis/RedisTest.java
@@ -0,0 +1,6 @@
+package com.ling.vhr.redis;/**
+ * @author zhangling
+ * @date 2022/6/27 3:47 PM
+ */
+public class RedisTest {
+}
