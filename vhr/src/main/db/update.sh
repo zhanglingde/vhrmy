cae2873c12eba75b38ba08e592d28453522978eb
zhangling <390597591@qq.com>
2022-12-12 10:31:23 +0800

    移除 flowable 配置

--- vhr/src/main/db/update.sh
+++ vhr/src/main/db/update.sh
@@ -1,14 +1,7 @@
 ##!/usr/bin/env bash
 #
 ## 需要在本地liquibase的lib中添加mysql驱动  witch liquibase
-#liquibase   \
-#    --driver=com.mysql.cj.jdbc.Driver       \
-#    --changeLogFile=driver.xml              \
-#    --url=jdbc:mysql://192.168.95.142:3306/maycur-finance-operation-platform  \
-#    --username=plat_app   \
-#    --password=Maycur@2021 \
-#    --log-level=DEBUG \
-#   update
+
 
 
 # zhangling 本地  witch liquibase
